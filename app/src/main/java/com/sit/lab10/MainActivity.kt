15769f827677ad09017733f8879685ef128e359b
Alok Raj <cse.22bcse81@silicon.ac.in>
2024-07-06 15:03:55 +0530

    Lab 10 assignment

--- app/src/main/java/com/sit/lab10/MainActivity.kt
+++ app/src/main/java/com/sit/lab10/MainActivity.kt
@@ -3,27 +3,39 @@ package com.sit.lab10
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.*
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.sit.lab10.ui.theme.LAB10Theme
 import kotlinx.coroutines.launch
 
@@ -39,112 +51,293 @@ class MainActivity : ComponentActivity() {
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Center
                 ) {
-                    TextField(
-                        value = text,
-                        onValueChange = { text = it },
-                        label = { Text("This is a text field") }
-                    )
-                    StyledTextField()
-                    SimpleOutlinedTextFieldSample()
-                    BrushTextField()
-                    PasswordTextField()
-                    BottomSheetExample()
+//                    TextField(
+//                        value = text,
+//                        onValueChange = { text = it },
+//                        label = { Text("This is a text field") }
+//                    )
+//                    StyledTextField()
+//                    SimpleOutlinedTextFieldSample()
+//                    BrushTextField()
+//                    PasswordTextField()
+//                    BottomSheetExample()
+
+
+                    ScaffoldExample()
+                  //  DialogWithImage(onDismissRequest = {}, onConfirmation = {} ,painterResource(R.drawable.myimg),"This is my Image")
+
+                    val openAlertDialog = remember { mutableStateOf(false) }
+
+                    when {
+                        // ...
+                        openAlertDialog.value -> {
+                            AlertDialogExample(
+                                onDismissRequest = { openAlertDialog.value = false },
+                                onConfirmation = {
+                                    openAlertDialog.value = false
+                                    println("Confirmation registered") // Add logic here to handle confirmation.
+                                },
+                                dialogTitle = "Alert dialog example",
+                                dialogText = "This is an example of an alert dialog with buttons.",
+                                icon = Icons.Default.Info
+                            )
+                        }
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun StyledTextField() {
-    var value by remember { mutableStateOf("Hello\nWorld\nInvisible") }
+    @Composable
+    fun StyledTextField() {
+        var value by remember { mutableStateOf("Hello\nWorld\nInvisible") }
 
-    TextField(
-        value = value,
-        onValueChange = { value = it },
-        label = { Text("Styled text field") },
-        maxLines = 2,
-        textStyle = TextStyle(color = Color.Blue, fontWeight = FontWeight.Bold),
-        modifier = Modifier.padding(20.dp)
-    )
-}
-
-@Composable
-fun SimpleOutlinedTextFieldSample() {
-    var text by remember { mutableStateOf("") }
-
-    OutlinedTextField(
-        value = text,
-        onValueChange = { text = it },
-        label = { Text("Label") }
-    )
-}
-
-@Composable
-fun BrushTextField(modifier: Modifier = Modifier) {
-    var text by remember { mutableStateOf("") }
-    val brush = remember {
-        Brush.linearGradient(
-            colors = listOf(Color.Blue, Color.Yellow, Color.Magenta)
+        TextField(
+            value = value,
+            onValueChange = { value = it },
+            label = { Text("Styled text field") },
+            maxLines = 2,
+            textStyle = TextStyle(color = Color.Blue, fontWeight = FontWeight.Bold),
+            modifier = Modifier.padding(20.dp)
         )
     }
-    TextField(
-        value = text,
-        onValueChange = { text = it },
-        textStyle = TextStyle(brush = brush)
-    )
+
+    @Composable
+    fun SimpleOutlinedTextFieldSample() {
+        var text by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Label") }
+        )
+    }
+
+    @Composable
+    fun BrushTextField(modifier: Modifier = Modifier) {
+        var text by remember { mutableStateOf("") }
+        val brush = remember {
+            Brush.linearGradient(
+                colors = listOf(Color.Blue, Color.Yellow, Color.Magenta)
+            )
+        }
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            textStyle = TextStyle(brush = brush)
+        )
+    }
+
+    @Composable
+    fun PasswordTextField() {
+        var password by rememberSaveable { mutableStateOf("") }
+
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Enter password") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+        )
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun BottomSheetExample(modifier: Modifier = Modifier) {
+        val sheetState = rememberModalBottomSheetState()
+        val scope = rememberCoroutineScope()
+        var showBottomSheet by remember { mutableStateOf(false) }
+        Scaffold(
+            floatingActionButton = {
+                ExtendedFloatingActionButton(
+                    text = { Text("Show bottom sheet") },
+                    icon = { Icon(Icons.Filled.Add, contentDescription = null) },
+                    onClick = {
+                        showBottomSheet = true
+                    }
+                )
+            }
+        ) { contentPadding ->
+            if (showBottomSheet) {
+                ModalBottomSheet(
+                    onDismissRequest = { showBottomSheet = false },
+                    sheetState = sheetState
+                ) {
+
+                    Column(
+                        modifier = Modifier.height(300.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Button(onClick = {
+                            scope.launch { sheetState.hide() }.invokeOnCompletion {
+                                if (!sheetState.isVisible) {
+                                    showBottomSheet = false
+                                }
+                            }
+                        }) {
+                            Text("Hide bottom sheet")
+                        }
+
+                        Text(text = "I am inside Bottom Sheet")
+                    }
+                }
+            }
+        }
+    }
+
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun AlertDialogExample(
+        onDismissRequest: () -> Unit,
+        onConfirmation: () -> Unit,
+        dialogTitle: String,
+        dialogText: String,
+        icon: ImageVector,
+    ) {
+        AlertDialog(
+            icon = {
+                Icon(icon, contentDescription = "Example Icon")
+            },
+            title = {
+                Text(text = dialogTitle)
+            },
+            text = {
+                Text(text = dialogText)
+            },
+            onDismissRequest = {
+                onDismissRequest()
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        onConfirmation()
+                    }
+                ) {
+                    Text("Confirm")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        onDismissRequest()
+                    }
+                ) {
+                    Text("Dismiss")
+                }
+            }
+        )
+    }
 }
 
-@Composable
-fun PasswordTextField() {
-    var password by rememberSaveable { mutableStateOf("") }
 
-    TextField(
-        value = password,
-        onValueChange = { password = it },
-        label = { Text("Enter password") },
-        visualTransformation = PasswordVisualTransformation(),
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
-    )
+@Composable
+fun DialogWithImage(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    painter: Painter,
+    imageDescription: String,
+) {
+    Dialog(onDismissRequest = { onDismissRequest() }) {
+        // Draw a rectangle shape with rounded corners inside the dialog
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(375.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Image(
+                    painter = painter,
+                    contentDescription = imageDescription,
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier
+                        .height(160.dp)
+                )
+                Text(
+                    text = imageDescription,
+                    modifier = Modifier.padding(16.dp),
+                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    TextButton(
+                        onClick = { onDismissRequest() },
+                        modifier = Modifier.padding(8.dp),
+                    ) {
+                        Text("Dismiss")
+                    }
+                    TextButton(
+                        onClick = { onConfirmation() },
+                        modifier = Modifier.padding(8.dp),
+                    ) {
+                        Text("Confirm")
+                    }
+                }
+            }
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BottomSheetExample(modifier: Modifier = Modifier) {
-    val sheetState = rememberModalBottomSheetState()
-    val scope = rememberCoroutineScope()
-    var showBottomSheet by remember { mutableStateOf(false) }
+fun ScaffoldExample() {
+    var presses by remember { mutableIntStateOf(0) }
+
     Scaffold(
-        floatingActionButton = {
-            ExtendedFloatingActionButton(
-                text = { Text("Show bottom sheet") },
-                icon = { Icon(Icons.Filled.Add, contentDescription = null) },
-                onClick = {
-                    showBottomSheet = true
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Top app bar")
                 }
             )
-        }
-    ) { contentPadding ->
-        if (showBottomSheet) {
-            ModalBottomSheet(
-                onDismissRequest = { showBottomSheet = false },
-                sheetState = sheetState
+        },
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary,
             ) {
-
-                Column(modifier = Modifier.height(300.dp), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-                    Button(onClick = {
-                        scope.launch { sheetState.hide() }.invokeOnCompletion {
-                            if (!sheetState.isVisible) {
-                                showBottomSheet = false
-                            }
-                        }
-                    }) {
-                        Text("Hide bottom sheet")
-                    }
-                    
-                    Text(text = "I am inside Bottom Sheet")
-                }
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = "Bottom app bar",
+                )
             }
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { presses++ }) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
+            }
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text =
+                """
+                    This is my scaffold. 
+
+                    You have pressed the floating action button $presses times.
+                """.trimIndent(),
+            )
         }
     }
 }
